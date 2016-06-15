0f775c35c5889711b03965cbd591bbaa899fc1f1
backeliten <jonas.back@swetune.se>
2016-06-15 10:41:48 +0200

    Update to support KeyOn event

--- display/display.ino
+++ display/display.ino
@@ -43,6 +43,8 @@ CAN_message_t   message_out;    //Structure for CAN message
 
 CAN_filter_t    filter, filter2, filter3, filter4;
 
+int16_t       tempvalue;
+uint8_t       KeyOn = 0;
 
 #define NUMFLAKES 10
 #define XPOS 0
@@ -51,16 +53,15 @@ CAN_filter_t    filter, filter2, filter3, filter4;
 
 volvo_id_s  rpm {.adress=0x155, .data_offset=4, .data_length=2, .multiplier=1.0, .value_offset=0};
 volvo_id_s  speed {.adress=0x145, .data_offset=6, .data_length=2, .multiplier=1.0, .value_offset=0};
-volvo_id_s  clt {.adress=0x4C5, .data_offset=5, .data_length=1, .multiplier=1.5, .value_offset=60};
+volvo_id_s  clt {.adress=0x4C5, .data_offset=5, .data_length=1, .multiplier=1.0, .value_offset=60};
 
-int16_t  volvo_convert_data(volvo_id_s conv_s, CAN_message_t message_in);   
+int16_t  volvo_convert_data(volvo_id_s conv_s, CAN_message_t message_in, int16_t *value);   
 
 #define LOGO16_GLCD_HEIGHT 16 
 #define LOGO16_GLCD_WIDTH  16 
 
-int16_t volvo_convert_data(volvo_id_s conv_s, CAN_message_t message_in)
+int16_t volvo_convert_data(volvo_id_s conv_s, CAN_message_t message_in, int16_t *value)
 {
-  int16_t converted = 0;
   float tempvalue = 0;
   if(message_in.id == conv_s.adress)
   {
@@ -73,9 +74,11 @@ int16_t volvo_convert_data(volvo_id_s conv_s, CAN_message_t message_in)
         tempvalue = (float)((message_in.buf[conv_s.data_offset] << 8) | message_in.buf[conv_s.data_offset+1]);   //Bitshift with 8 and or lower
       }
       tempvalue = tempvalue - (float)conv_s.value_offset;   //Correct with offset
-      converted = (int16_t)(tempvalue*conv_s.multiplier);   //Correct with multiplier   
+      *value = (int16_t)(tempvalue*conv_s.multiplier);   //Correct with multiplier   
+
+      return 0;
   }
-  return converted;
+  return -1;
 }
 
 static const unsigned char PROGMEM logo16_glcd_bmp[] =
@@ -103,145 +106,45 @@ static const unsigned char PROGMEM logo16_glcd_bmp[] =
 void setup()   {                
   Serial.begin(1000000);
 
-  filter.ext = 0;
-  filter.id = rpm.adress;
+ // filter.ext = 0;
+ // filter.id = rpm.adress;
 
-  filter2.ext = 0;
-  filter2.id = speed.adress;
+//  filter2.ext = 0;
+//  filter2.id = speed.adress;
 
-  filter3.ext = 0;
-  filter3.id = clt.adress;
+//  filter3.ext = 0;
+//  filter3.id = clt.adress;
 
-  can.begin(filter);
+  can.begin();
 
-  can.setFilter(filter, 0);
-   can.setFilter(filter2, 1);
-    can.setFilter(filter3, 2);
-     can.setFilter(filter, 3);
-      can.setFilter(filter, 4);
-       can.setFilter(filter, 5);
-        can.setFilter(filter, 6);
-         can.setFilter(filter, 7);
+//  can.setFilter(filter3, 0);
+//   can.setFilter(filter3, 1);
+//    can.setFilter(filter3, 2);
+//     can.setFilter(filter3, 3);
+//      can.setFilter(filter3, 4);
+//       can.setFilter(filter3, 5);
+//        can.setFilter(filter3, 6);
+//         can.setFilter(filter3, 7);
   
 
   // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
-  // init done
-  
-  // Show image buffer on the display hardware.
-  // Since the buffer is intialized with an Adafruit splashscreen
-  // internally, this will display the splashscreen.
- // display.display();
- // delay(2000);
 
   // Clear the buffer.
   display.clearDisplay();
-
-  // draw a single pixel
-//  display.drawPixel(10, 10, WHITE);
-  // Show the display buffer on the hardware.
-  // NOTE: You _must_ call display after making any drawing commands
-  // to make them visible on the display hardware!
-
+  
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0,0);
   display.println("Volvo ext.display..");
   display.display();
   delay(2000);
-//  display.clearDisplay();
 
-  // draw many lines
- // testdrawline();
- // display.display();
- // delay(2000);
- // display.clearDisplay();
-
-  // draw rectangles
- // testdrawrect();
-//  display.display();
-///  delay(2000);
- // display.clearDisplay();
-
-  // draw multiple rectangles
- // testfillrect();
- // display.display();
- // delay(2000);
- // display.clearDisplay();
-
-  // draw mulitple circles
-//  testdrawcircle();
-//  display.display();
-//  delay(2000);
-///  display.clearDisplay();
-
-  // draw a white circle, 10 pixel radius
-///  display.fillCircle(display.width()/2, display.height()/2, 10, WHITE);
- // display.display();
- // delay(2000);
- // display.clearDisplay();
-
- // testdrawroundrect();
- // delay(2000);
- // display.clearDisplay();
-
- // testfillroundrect();
- // delay(2000);
- // display.clearDisplay();
-
- // testdrawtriangle();
-//  delay(2000);
- // display.clearDisplay();
-   
- // testfilltriangle();
- // delay(2000);
- // display.clearDisplay();
-
-  // draw the first ~12 characters in the font
-//  testdrawchar();
-//  display.display();
-//  delay(2000);
-//  display.clearDisplay();
-
-  // draw scrolling text
- // testscrolltext();
- // delay(2000);
- // display.clearDisplay();
-
-  // text display tests
-//  display.setTextSize(1);
-//  display.setTextColor(WHITE);
-//  display.setCursor(0,0);
-//  display.println("Hello, world!");
-//  display.setTextColor(BLACK, WHITE); // 'inverted' text
- // display.println(3.141592);
-//  display.setTextSize(2);
-//  display.setTextColor(WHITE);
-//  display.print("0x"); display.println(0xDEADBEEF, HEX);
-//  display.display();
-//  delay(2000);
-
-  // miniature bitmap display
- // display.clearDisplay();
-//  display.drawBitmap(30, 16,  logo16_glcd_bmp, 16, 16, 1);
-//  display.display();
-
-  // invert the display
-//  display.invertDisplay(true);
-//  delay(1000); 
-//  display.invertDisplay(false);
-//  delay(1000); 
-
-  // draw a bitmap icon and 'animate' movement
-//  testdrawbitmap(logo16_glcd_bmp, LOGO16_GLCD_HEIGHT, LOGO16_GLCD_WIDTH);
-//  display.clearDisplay();
  display.setTextSize(7);
  display.setTextColor(WHITE);
 
  Serial.println("Volvo Extended display startup....");
 }
-
-
 void loop() {
 
  // Serial.println(message_in.id);
@@ -258,15 +161,19 @@ void loop() {
  
   if(can.read(message_in))
   {
-    
-    
-    display.clearDisplay();
-
-  //    if(message_in.id = 0x155)   
-  //    {
-  //      
-   //   }
-  
+      if(message_in.id == 0x035)   
+      {
+          if((message_in.buf[0] & 0x0F) == 0x04)
+          {
+            KeyOn = 1;      //Key is on!
+          }
+          else
+          {
+            KeyOn = 0;    //Key is off!
+            
+          }
+      } 
+      
   //  display.setCursor(0,0);
   //  display.print("ID:");
   //  display.print(message_in.id);
@@ -275,201 +182,26 @@ void loop() {
     //display.print("Len:");
     //display.print(message_in.len);
 
-    display.setCursor(0,0);
-    //display.print("D:");
-    display.print(volvo_convert_data(clt, message_in));
-    
-   // Serial.println(message_in.id);
-    display.display();
-  }
+      if(volvo_convert_data(clt, message_in, &tempvalue) == 0)
+      {
+          if(KeyOn == 1)
+          {
+            display.clearDisplay();
+            display.setCursor(0,0);
+            display.print(tempvalue);
+            display.display();
+          }
+          else
+          {
+            display.clearDisplay();
+            display.display();
+          }
+       }
 
-  Serial.println("Loop...");     // string
+       //display.print("D:");
+   // Serial.println(message_in.id);
+
+  }
 
   delay(1);
-}
-
-
-void testdrawbitmap(const uint8_t *bitmap, uint8_t w, uint8_t h) {
-  uint8_t icons[NUMFLAKES][3];
- 
-  // initialize
-  for (uint8_t f=0; f< NUMFLAKES; f++) {
-    icons[f][XPOS] = random(display.width());
-    icons[f][YPOS] = 0;
-    icons[f][DELTAY] = random(5) + 1;
-    
-    Serial.print("x: ");
-    Serial.print(icons[f][XPOS], DEC);
-    Serial.print(" y: ");
-    Serial.print(icons[f][YPOS], DEC);
-    Serial.print(" dy: ");
-    Serial.println(icons[f][DELTAY], DEC);
-  }
-
-  while (1) {
-    // draw each icon
-    for (uint8_t f=0; f< NUMFLAKES; f++) {
-      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], logo16_glcd_bmp, w, h, WHITE);
-    }
-    display.display();
-    delay(200);
-    
-    // then erase it + move it
-    for (uint8_t f=0; f< NUMFLAKES; f++) {
-      display.drawBitmap(icons[f][XPOS], icons[f][YPOS],  logo16_glcd_bmp, w, h, BLACK);
-      // move it
-      icons[f][YPOS] += icons[f][DELTAY];
-      // if its gone, reinit
-      if (icons[f][YPOS] > display.height()) {
-	icons[f][XPOS] = random(display.width());
-	icons[f][YPOS] = 0;
-	icons[f][DELTAY] = random(5) + 1;
-      }
-    }
-   }
-}
-
-
-void testdrawchar(void) {
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0,0);
-
-  for (uint8_t i=0; i < 168; i++) {
-    if (i == '\n') continue;
-    display.write(i);
-    if ((i > 0) && (i % 21 == 0))
-      display.println();
-  }    
-  display.display();
-}
-
-void testdrawcircle(void) {
-  for (int16_t i=0; i<display.height(); i+=2) {
-    display.drawCircle(display.width()/2, display.height()/2, i, WHITE);
-    display.display();
-  }
-}
-
-void testfillrect(void) {
-  uint8_t color = 1;
-  for (int16_t i=0; i<display.height()/2; i+=3) {
-    // alternate colors
-    display.fillRect(i, i, display.width()-i*2, display.height()-i*2, color%2);
-    display.display();
-    color++;
-  }
-}
-
-void testdrawtriangle(void) {
-  for (int16_t i=0; i<min(display.width(),display.height())/2; i+=5) {
-    display.drawTriangle(display.width()/2, display.height()/2-i,
-                     display.width()/2-i, display.height()/2+i,
-                     display.width()/2+i, display.height()/2+i, WHITE);
-    display.display();
-  }
-}
-
-void testfilltriangle(void) {
-  uint8_t color = WHITE;
-  for (int16_t i=min(display.width(),display.height())/2; i>0; i-=5) {
-    display.fillTriangle(display.width()/2, display.height()/2-i,
-                     display.width()/2-i, display.height()/2+i,
-                     display.width()/2+i, display.height()/2+i, WHITE);
-    if (color == WHITE) color = BLACK;
-    else color = WHITE;
-    display.display();
-  }
-}
-
-void testdrawroundrect(void) {
-  for (int16_t i=0; i<display.height()/2-2; i+=2) {
-    display.drawRoundRect(i, i, display.width()-2*i, display.height()-2*i, display.height()/4, WHITE);
-    display.display();
-  }
-}
-
-void testfillroundrect(void) {
-  uint8_t color = WHITE;
-  for (int16_t i=0; i<display.height()/2-2; i+=2) {
-    display.fillRoundRect(i, i, display.width()-2*i, display.height()-2*i, display.height()/4, color);
-    if (color == WHITE) color = BLACK;
-    else color = WHITE;
-    display.display();
-  }
-}
-   
-void testdrawrect(void) {
-  for (int16_t i=0; i<display.height()/2; i+=2) {
-    display.drawRect(i, i, display.width()-2*i, display.height()-2*i, WHITE);
-    display.display();
-  }
-}
-
-void testdrawline() {  
-  for (int16_t i=0; i<display.width(); i+=4) {
-    display.drawLine(0, 0, i, display.height()-1, WHITE);
-    display.display();
-  }
-  for (int16_t i=0; i<display.height(); i+=4) {
-    display.drawLine(0, 0, display.width()-1, i, WHITE);
-    display.display();
-  }
-  delay(250);
-  
-  display.clearDisplay();
-  for (int16_t i=0; i<display.width(); i+=4) {
-    display.drawLine(0, display.height()-1, i, 0, WHITE);
-    display.display();
-  }
-  for (int16_t i=display.height()-1; i>=0; i-=4) {
-    display.drawLine(0, display.height()-1, display.width()-1, i, WHITE);
-    display.display();
-  }
-  delay(250);
-  
-  display.clearDisplay();
-  for (int16_t i=display.width()-1; i>=0; i-=4) {
-    display.drawLine(display.width()-1, display.height()-1, i, 0, WHITE);
-    display.display();
-  }
-  for (int16_t i=display.height()-1; i>=0; i-=4) {
-    display.drawLine(display.width()-1, display.height()-1, 0, i, WHITE);
-    display.display();
-  }
-  delay(250);
-
-  display.clearDisplay();
-  for (int16_t i=0; i<display.height(); i+=4) {
-    display.drawLine(display.width()-1, 0, 0, i, WHITE);
-    display.display();
-  }
-  for (int16_t i=0; i<display.width(); i+=4) {
-    display.drawLine(display.width()-1, 0, i, display.height()-1, WHITE); 
-    display.display();
-  }
-  delay(250);
-}
-
-void testscrolltext(void) {
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  display.setCursor(10,0);
-  display.clearDisplay();
-  display.println("scroll");
-  display.display();
- 
-  display.startscrollright(0x00, 0x0F);
-  delay(2000);
-  display.stopscroll();
-  delay(1000);
-  display.startscrollleft(0x00, 0x0F);
-  delay(2000);
-  display.stopscroll();
-  delay(1000);    
-  display.startscrolldiagright(0x00, 0x07);
-  delay(2000);
-  display.startscrolldiagleft(0x00, 0x07);
-  delay(2000);
-  display.stopscroll();
 }
